27c7f173fecb11f37be5b8d4d72a18c9010773a9
Alejo <froy2004xd@gmail.com>
2025-01-09 15:38:46 -0500

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+const $abrir = document.querySelector("#BtnAbrir")
+const $cerrar = document.querySelector(".BtnCerrar")
+const $inicio = document.querySelector(".inicio")
+
+function abrir() {
+    $inicio.classList.add("visible")
+    $abrir.classList.add("ocultar")
+}
+function cerrar() {
+    $inicio.classList.remove("visible")
+    $abrir.classList.remove("ocultar")
+}
+$abrir.addEventListener("click",abrir)
+$cerrar.addEventListener("click",cerrar)
